ab5733bed43d9e4c1394664ec657fdb8ea508379
Mustafa Akgul <mustafakgl@proton.me>
2024-11-30 10:08:01 +0300

    Add raw Trivy output files

--- server.js
+++ server.js
@@ -1,4 +1,1 @@
 const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Merhaba DevSecOps!'));
-app.listen(3000, () => console.log('Sunucu 3000 portunda çalışıyor...'));
